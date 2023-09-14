9e1061989c8f78e2697d7c8927b1f5ef7f043e61
saidureg <saidureg@gmail.com>
2023-09-14 10:41:09 +0600

    fetch json data and show on card in display

---
+++ src/component/cards/Cards.jsx
@@ -0,0 +1,11 @@
+import Card from "../card/Card";
+
+const Cards = () => {
+  return (
+    <div>
+      <Card></Card>
+    </div>
+  );
+};
+
+export default Cards;
--- src/component/gallery/Gallery.jsx
+++ src/component/gallery/Gallery.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import "./Gallery.css";
+import { useEffect } from "react";
+import Cards from "../cards/Cards";
 const Gallery = () => {
-  return <div className="gallery-container"></div>;
+  const [dataLoad, setDataLoad] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setDataLoad(data));
+  }, []);
+  return (
+    <div className="gallery-container">
+      {dataLoad.map((players, idx) => (
+        <Cards key={idx}></Cards>
+      ))}
+    </div>
+  );
 };
 
 export default Gallery;
